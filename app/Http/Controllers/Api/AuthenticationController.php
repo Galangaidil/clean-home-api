547b3b8d165e978ec276a9c1da1a2032081174e6
Galang Aidil Akbar <73085251+Galangaidil@users.noreply.github.com>
2023-01-15 18:09:37 +0700

    Memperbaiki Hash check password

--- app/Http/Controllers/Api/AuthenticationController.php
+++ app/Http/Controllers/Api/AuthenticationController.php
@@ -21,7 +21,7 @@ class AuthenticationController extends Controller
     {
         $user = User::where('email', $request->input('email'))->first();
 
-        if (!$user || Hash::check($request->input('password'), $user->password)){
+        if (! $user || ! Hash::check($request->input('password'), $user->password)) {
             throw ValidationException::withMessages([
                 "email" => ['The provided credentials are incorrect.']
             ]);
