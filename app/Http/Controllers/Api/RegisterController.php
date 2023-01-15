9a2d047739b0dc873941cd7ae78a9c574e18d289
Galang Aidil Akbar <73085251+Galangaidil@users.noreply.github.com>
2023-01-15 17:35:25 +0700

    Menambahkan method untuk menyimpan data user

--- app/Http/Controllers/Api/RegisterController.php
+++ app/Http/Controllers/Api/RegisterController.php
@@ -3,9 +3,24 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
-use Illuminate\Http\Request;
+use App\Http\Requests\Api\RegisterRequest;
+use App\Models\User;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Support\Facades\Hash;
 
 class RegisterController extends Controller
 {
-    //
+    public function store(RegisterRequest $request):JsonResponse
+    {
+        $user = User::create([
+            'name' => $request->input('name'),
+            'email' => $request->input('email'),
+            'password' => Hash::make($request->input('password'))
+        ]);
+
+        return response()->json([
+            "message" => "Registrasi berhasil.",
+            "user" => $user
+        ], 201);
+    }
 }
