d4fdc3da0aa09b00df782f5fc69cde76a19aadb1
Galang Aidil Akbar <73085251+Galangaidil@users.noreply.github.com>
2023-01-22 13:55:06 +0700

    Add Login test with invalid password

--- tests/Feature/Api/LoginTest.php
+++ tests/Feature/Api/LoginTest.php
@@ -8,17 +8,35 @@ use Tests\TestCase;
 
 class LoginTest extends TestCase
 {
+    private User $user;
+
     use RefreshDatabase;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+        $this->user = User::factory()->create();
+    }
 
     public function test_user_can_authenticated_using_api()
     {
-        $user = User::factory()->create();
-
         $response = $this->postJson(route('v1.login'), [
-            'email' => $user->email,
+            'email' => $this->user->email,
             'password' => 'password',
         ]);
 
         $response->assertJson(['message' => 'Login berhasil.']);
     }
+
+    public function test_user_can_not_authenticated_with_invalid_password()
+    {
+        $response = $this->postJson(route('v1.login'), [
+            'email' => $this->user->email,
+            'password' => 'wrong password'
+        ]);
+
+        $response->assertUnprocessable();
+    }
+
+
 }
